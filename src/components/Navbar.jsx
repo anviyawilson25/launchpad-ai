aa76544c4a9513f582404075d335e1fe1176c0c6
Renna Rose <rennarose585@gmail.com>
2026-07-17 22:17:46 +0530

    Add components and pages

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>LaunchPad AI</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Features</li>
+        <li>About</li>
+        <li>Login</li>
+      </ul>
+
+      <button>Get Started</button>
+    </nav>
+  );
+}
+
+export default Navbar;
